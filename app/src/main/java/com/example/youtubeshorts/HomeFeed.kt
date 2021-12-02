2745d8d9a4d824a19d85fe7e115ce3123aea9d64
josephvasconcelos <joseph.vasconcelos97@gmail.com>
2021-12-02 13:32:50 -0500

    added first video

--- app/src/main/java/com/example/youtubeshorts/HomeFeed.kt
+++ app/src/main/java/com/example/youtubeshorts/HomeFeed.kt
@@ -3,11 +3,27 @@ package com.example.youtubeshorts
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.MediaController
+import android.widget.VideoView
+import android.net.Uri
+import android.net.Uri.*
+import android.os.Environment
 import androidx.appcompat.app.AppCompatActivity
 
 class HomeFeed: AppCompatActivity()  {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.home_feed)
+        val videoView = findViewById<VideoView>(R.id.videoView)
+        //Creating MediaController
+        val mediaController = MediaController(this)
+        mediaController.setAnchorView(videoView)
+        //specify the location of media file
+        val uri:Uri = parse("android.resource://" + getPackageName() +"/"+R.raw.video1)
+        //Setting MediaController and URI, then starting the videoView
+        videoView.setMediaController(mediaController)
+        videoView.setVideoURI(uri)
+        videoView.requestFocus()
+        videoView.start()
     }
 }
