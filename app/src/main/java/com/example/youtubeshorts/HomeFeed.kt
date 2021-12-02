e7863c5b435a95bf9af3da09c997fb59dfae541e
josephvasconcelos <joseph.vasconcelos97@gmail.com>
2021-12-02 13:12:47 -0500

    added login setup

---
+++ app/src/main/java/com/example/youtubeshorts/HomeFeed.kt
@@ -0,0 +1,13 @@
+package com.example.youtubeshorts
+
+import android.content.Intent
+import android.os.Bundle
+import android.widget.Button
+import androidx.appcompat.app.AppCompatActivity
+
+class HomeFeed: AppCompatActivity()  {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.home_feed)
+    }
+}
--- app/src/main/java/com/example/youtubeshorts/MainActivity.kt
+++ app/src/main/java/com/example/youtubeshorts/MainActivity.kt
@@ -1,11 +1,27 @@
 package com.example.youtubeshorts
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.home_feed)
+        setContentView(R.layout.activity_main)
+        val username = findViewById(R.id.et_user_name) as EditText
+        val password = findViewById(R.id.et_password) as EditText
+        findViewById<Button>(R.id.btn_login).setOnClickListener {
+            if ( username.text.toString() == "jung" && password.text.toString() == "comp415"){
+                val intent = Intent(this, HomeFeed::class.java)
+                startActivity(intent)
+            }
+            else{
+                val toast: Toast = Toast.makeText(this ,"invalid login", Toast.LENGTH_LONG)
+                toast.show()
+            }
+        }
     }
 }
